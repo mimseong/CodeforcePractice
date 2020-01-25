e429fc6efa80cce2c71d1001583ef90b19ae0145
mimseong <parkseongmin98@gmail.com>
2020-01-25 23:34:46 +0900

    feat: Add problem E. The Meeting Place Cannot Be Changed

--- Practice8_191228/E_The_Meeting_Place_Cannot_Be_Changed.cpp
+++ Practice8_191228/E_The_Meeting_Place_Cannot_Be_Changed.cpp
@@ -2,37 +2,78 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-using i64 = long long int;
+
+bool sortbyFirst(pair<int,int> &a, pair<int,int> &b){
+    return (a.first < b.first);
+}
+
+bool isOk(double mid, int n, vector<pair<int, int>> v){
+    double val1_left = v[0].first;
+    double val1_right = v[0].first + v[0].second*mid;
+    
+    printf("\nval1 : %lf %lf\n", val1_left, val1_right);
+    
+    for(int i = 1; i < n; i++){
+        double val2_left = max((double)v[0].first, (double)v[i].first - v[i].second*mid);
+        double val2_right = min((double)v[n-1].first, (double)v[i].first + v[i].second*mid);
+        printf("val2 : %lf %lf\n", val2_left, val2_right);
+        
+        if(val1_left < val2_left)
+            val1_left = val2_left;
+        if(val2_right < val1_right)
+            val1_right = val2_right;
+        
+        if((val1_left < val2_left) && (val2_left > val1_right))
+            return false;
+        if((val1_left > val2_left) && (val1_left > val2_right))
+            return false;
+    }
+    
+    return true;
+}
 
 int main(){
     int n;
     scanf("%d", &n);
-    vector<pair<i64, i64>> v(n);
+    vector<pair<int, int>> v(n);
     
+    //position
     for(int i = 0; i < n; i++)
-        scanf("%lld", &v[i].first);
+        scanf("%d", &v[i].first);
+    //speed
     for(int i = 0; i < n; i++)
-        scanf("%lld", &v[i].second);
+        scanf("%d", &v[i].second);
     
-    double lo = 0, hi = 1e9;
-    
-    for(int i = 0; i < 100; i++){
-        double mid = (lo + hi) * 0.5;
-        
-        vector<double> starts, ends;
-        for(int j = 0; j < n; j++){
-            starts.push_back(v[j].first - v[j].second * min((double)v[j].first / v[j].second, mid));
-            ends.push_back(v[j].first + v[j].second * min((1e9 - v[j].first) / v[j].second, mid));
-        }
-        
-        if(*max_element(starts.begin(), starts.end()) > *min_element(ends.begin(), ends.end()))
-            lo = mid;
+    sort(v.begin(), v.end(), sortbyFirst);
+    double max_time = 0;
+    for(int i = 0; i < n; i++){
+        double time_tmp;
+        if(v[i].first - v[0].first < v[n-1].first - v[i].first)
+            time_tmp = (v[n-1].first - v[i].first)/v[i].second;
         else
-            hi = mid;
+            time_tmp = (v[i].first - v[0].first)/v[i].second;
+        if(time_tmp > max_time)
+            max_time = time_tmp;
     }
     
-    printf("%.10lf", lo);
+    double lo = 0, hi = max_time;
+    double ans = 0;
     
+    for(int i = 0; i < 5; i++){
+        double mid = (lo+hi)/2;
+        printf("%lf %lf %lf", mid, lo, hi);
+        if(isOk(mid, n, v)){
+            hi = mid;
+            ans = mid;
+            printf(" ok\n");
+        }
+        else{
+            lo = mid;
+            printf(" no\n");
+        }
+    }
+    
+    printf("%lf", ans);
     
     return 0;
 }
