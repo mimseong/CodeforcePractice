4099bf9f72932a63d1aa4730fddca46f6a0e73ae
mimseong <parkseongmin98@gmail.com>
2020-03-13 19:20:04 +0900

    feat: Add problem C. Save the problem

---
+++ Practice13_200309/C_Save_the_problem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    
+    printf("%d %d\n", 2 * (a - 1), 2);
+    printf("%d %d", 1, 2);
+    
+    return 0;
+}
