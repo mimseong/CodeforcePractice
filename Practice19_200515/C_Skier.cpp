16351d37bb168f414cf8046a4418bacd2b411f74
mimseong <parkseongmin98@gmail.com>
2020-05-20 14:43:00 +0900

    Uadate : C. Skier

--- Practice19_200515/C_Skier.cpp
+++ Practice19_200515/C_Skier.cpp
@@ -7,137 +7,55 @@
 #include <queue>
 #include <stack>
 #include <set>
-#include <map>
  
 using namespace std;
 using i64 = long long;
- 
-int setSNWE(int n, char t)
+using ii = pair<int, int>;
+
+void    calc()
 {
-    if (t == 'W')
-    {
-        if (n%100/10 == 0)
-            n += 10;
-    }
-    else if (t == 'E')
-    {
-        if (n%10 == 0)
-            n += 1;
-    }
-    else if (t == 'S')
-    {
-        if (n/1000 == 0)
-            n += 1000;
-    }
-    else if (t == 'N')
-    {
-        if (n%1000/100 == 0)
-            n += 100;
-    }
-    return n;
-}
- 
-int main() 
-{
-    int t;
-    scanf("%d", &t);
+    string s;
+    cin >> s;
     
-    for (int i = 0; i < t; i++)
+    int x = 0, y = 0;
+    set<pair<ii, ii>> visited;
+    int ans = 0;
+    
+    for (int i = 0; i < s.size(); i++)
     {
-        string s;
-        cin >> s;
+        int nx = x, ny = y;
         
-        map<pair<int, int>, int> idx;
-        int x = 0, y = 0;
-        int count = 0;
+        if (s[i] == 'N')
+            ny++;
+        if (s[i] == 'S')
+            ny--;
+        if (s[i] == 'E')
+            nx++;
+        if (s[i] == 'W')
+            nx--;
+            
+        if (visited.find(pair<ii, ii>(ii(x, y), ii(nx, ny))) != visited.end())
+            ans++;
+        else
+            ans += 5;
         
-        idx[{0, 0}] = 0;
-        for (int j = 1; j <= s.size(); j++)
-        {
-            //cout << s[j-1] << endl;
-            int bfr_x = x, bfr_y = y;
-            if (s[j-1] == 'W')
-                x--;
-            else if (s[j-1] == 'E')
-                x++;
-            else if (s[j-1] == 'S')
-                y--;
-            else if (s[j-1] == 'N')
-                y++;
-            
-            idx[{bfr_x, bfr_y}] = setSNWE(idx[{bfr_x, bfr_y}], s[j-1]);
-            //cout << bfr_x << " " << bfr_y << " " << idx[{bfr_x, bfr_y}] << endl;
-            
-            //없는 경우
-            if (idx.find({x, y}) == idx.end())
-            {
-                if (s[j-1] == 'W')
-                    idx[{x, y}] = setSNWE(idx[{x, y}], 'E');
-                else if (s[j-1] == 'E')
-                    idx[{x, y}] = setSNWE(idx[{x, y}], 'W');
-                else if (s[j-1] == 'S')
-                    idx[{x, y}] = setSNWE(idx[{x, y}], 'N');
-                else if (s[j-1] == 'N')
-                    idx[{x, y}] = setSNWE(idx[{x, y}], 'S');
-                count += 5;
-                //cout << "없어용" << endl;
-            }
-            //있는 경우
-            else
-            {
-                bool is_true = false;
-            
-                if (s[j-1] == 'W')
-                {
-                    if (idx[{x, y}] == setSNWE(idx[{x, y}], 'E'))
-                    {
-                        count += 1;
-                        is_true = true;
-                    }
-                }
-                else if (s[j-1] == 'E')
-                {
-                    if (idx[{x, y}] == setSNWE(idx[{x, y}], 'W'))
-                    {
-                        count += 1;
-                        is_true = true;
-                    }
-                }
-                else if (s[j-1] == 'S')
-                {
-                    if (idx[{x, y}] == setSNWE(idx[{x, y}], 'N'))
-                    {
-                        count += 1;
-                        is_true = true;
-                    }
-                }
-                else if (s[j-1] == 'N')
-                {
-                    if (idx[{x, y}] == setSNWE(idx[{x, y}], 'S'))
-                    {
-                        count += 1;
-                        is_true = true;
-                    }
-                }
-                if (is_true == false)
-                {
-                    if (s[j-1] == 'W')
-                        idx[{x, y}] = setSNWE(idx[{x, y}], 'E');
-                    else if (s[j-1] == 'E')
-                        idx[{x, y}] = setSNWE(idx[{x, y}], 'W');
-                    else if (s[j-1] == 'S')
-                        idx[{x, y}] = setSNWE(idx[{x, y}], 'N');
-                    else if (s[j-1] == 'N')
-                        idx[{x, y}] = setSNWE(idx[{x, y}], 'S');
-                    count += 5;
-                }
-            }
-            //cout << x << " " << y << endl;
-            //cout << x << " " << y << " " << idx[{x, y}] << endl;
-        }
+        visited.insert(pair<ii, ii>(ii(x,y), ii(nx,ny)));
+        visited.insert(pair<ii, ii>(ii(nx,ny), ii(x,y)));
         
-        cout << count << endl;
+        x = nx;
+        y = ny;
     }
+    
+    cout << ans << '\n';
+}
+
+int     main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++)
+        calc();
     
     return 0;
 }
