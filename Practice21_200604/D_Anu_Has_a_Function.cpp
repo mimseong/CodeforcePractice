4573ca34209ff52a83c56c92337cfece38c6145e
mimseong <parkseongmin98@gmail.com>
2020-06-30 22:29:32 +0900

    Update D_Anu_Has_a_Function.cpp

--- Practice21_200604/D_Anu_Has_a_Function.cpp
+++ Practice21_200604/D_Anu_Has_a_Function.cpp
@@ -16,40 +16,36 @@ using i64 = long long;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
 
-bool sortbyfirst(const pair<int,int> &a, const pair<int,int> &b) 
-{ 
-    return (a.second < b.second); 
-}
-
-int count_one(int n)
+int main()
 {
-    int count = 0;
-
-    for (i64 i = 1; i <= n; i *= 2)
-    {
-        int res =  n & i;
-        if (res == i)
-          count++;
-    }
-
-    return count;
-}
-
-
-int main() {
     int n;
     scanf("%d", &n);
     
-    vector<ii> v(n);
+    vector<i64> v(n);
+    i64 sum = 0;
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &v[i].first);
-        v[i].second = count_one(v[i].first);
+        scanf("%lld", &v[i]);
+        sum |= v[i];
     }
 
-    sort(v.begin(), v.end(), sortbyfirst);
+    i64 max = 0;
+    i64 max_idx = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (sum - (sum - v[i]) > max)
+        {
+            max = sum - (sum - v[i]);
+            max_idx = i;
+        }
+    }
 
-    
+    printf("%d ", v[max_idx]);
+    for (int i = 0; i < n; i++)
+    {
+        if (i != max_idx)
+            printf("%d ", v[i]);
+    }
     
     return 0;
 }
