05bc12eff87c27c5f7debf354dc097b1089a5bc2
mimseong <parkseongmin98@gmail.com>
2020-06-30 23:32:21 +0900

    Update D_Anu_Has_a_Function.cpp

--- Practice21_200604/D_Anu_Has_a_Function.cpp
+++ Practice21_200604/D_Anu_Has_a_Function.cpp
@@ -10,32 +10,40 @@
 #include <map>
 #define xx first
 #define yy second
-
+ 
 using namespace std;
 using i64 = long long;
 using ii = pair<int, int>;
 using ii64 = pair<i64, i64>;
-
+ 
 int main()
 {
     int n;
     scanf("%d", &n);
     
     vector<i64> v(n);
-    i64 sum = 0;
+    i64 all = 0;
     for (int i = 0; i < n; i++)
     {
         scanf("%lld", &v[i]);
-        sum |= v[i];
+        all |= v[i];
     }
+ 
+    vector<i64> left(n, 0);
+    for (int i = 1; i < n; i++)
+        left[i] = left[i-1] | v[i-1];
 
-    i64 max = 0;
-    i64 max_idx = 0;
-    for (int i = 0; i < n; i++)
+    vector<i64> right(n, 0);
+    for (int i = n-1; i > 0; i--)
+        right[i-1] = right[i] | v[i];
+ 
+    int max_idx = 0;
+    i64 max = all - (left[0] | right[0]);
+    for (int i = 1; i < n; i++)
     {
-        if (sum - (sum - v[i]) > max)
+        if (all - (left[i] | right[i]) > max)
         {
-            max = sum - (sum - v[i]);
+            max = all - (left[i] | right[i]);
             max_idx = i;
         }
     }
